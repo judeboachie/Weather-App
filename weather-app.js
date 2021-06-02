24a540840f590e7d2256fa417de28bd897782e67
judeboachie <79732986+judeboachie@users.noreply.github.com>
2021-06-02 00:57:59 -0400

    Minor changes
    - Moved the "parseInt(input.value)" check to an earlier point
    - Placed the forecast items in an array for looping

--- weather-app.js
+++ weather-app.js
@@ -12,16 +12,19 @@ const tempDisplay = document.getElementById('tempDisplay');
 const windDisplay = document.getElementById('windDisplay');
 const descriptionDisplay = document.getElementById('descriptionDisplay');
 const forecastContainer = document.getElementById('forecastContainer');
-const forecast1Display = document.getElementById('forecast1Display');
-const forecast2Display = document.getElementById('forecast2Display');
-const forecast3Display = document.getElementById('forecast3Display');
 
 
 function getWeather(){
     loader.style.visibility = "visible";
     const location = input.value;
-    fetch(`https://goweather.herokuapp.com/weather/${location}`)
-    .then((data) => {
+    if(parseInt(input.value)){ //This API returns weather data for plain number entries (up to a certain point), but we don't want that
+        console.log(`Error. That's a number, not a place.`);
+        loader.style.visibility = "hidden";
+        displayContainer.style.display = "none";
+        errorDisplay.style.display = "block";
+    } else { 
+        fetch(`https://goweather.herokuapp.com/weather/${location}`)        
+        .then((data) => {
         return data.json();
     })
     .then((data => {
@@ -53,18 +56,10 @@ function getWeather(){
         displayContainer.style.display = "none";
         errorDisplay.style.display = "block";
     })
-}
-    function displayWeather(data){  
-        // This API actually has weather data for number entries (up to a certain point), but there's no documentation to actually explain why.
-        // If the user enters a number          
-        if(parseInt(input.value)){
-            console.log(`Error. That's a number, not a place.`);
-            loader.style.visibility = "hidden";
-            displayContainer.style.display = "none";
-            errorDisplay.style.display = "block";
-        } 
-        // If the user mispells the location name or enters some gibberish
-        else if(data.temperature == ""){
+}}
+    function displayWeather(data){ 
+        // If the user misspells the location name or enters some gibberish
+        if(data.temperature == ""){
             console.log(`Error. There doesn't seem to be any weather data for "${input.value}"`);
             loader.style.visibility = "hidden";
             displayContainer.style.display = "none";
@@ -75,13 +70,23 @@ function getWeather(){
             loader.style.visibility = "hidden";;
             errorDisplay.style.display = "none";
             displayContainer.style.display = "block";
+            forecastContainer.innerHTML = ""; //Refresh the forecast UI
 
             regionDisplay.innerHTML = `Here is today's weather for ${input.value}`;
             tempDisplay.innerHTML = data.temperature;
             windDisplay.innerHTML = data.wind;
-            descriptionDisplay.innerHTML = data.description;
-            forecast1Display.innerHTML = `~~~ Tomorrow ~~~  <br> Temperature: ${data.forecast[0].temperature} <br> Wind: ${data.forecast[0].wind} <br>`;
-            forecast2Display.innerHTML = `~~~ 2 Days from Now ~~~  <br> Temperature: ${data.forecast[1].temperature}  <br> Wind: ${data.forecast[1].wind} <br>`;
-            forecast3Display.innerHTML = `~~~ 3 Days from Now ~~~  <br> Temperature: ${data.forecast[2].temperature} <br> Wind: ${data.forecast[2].wind} <br>`;
-        }
+            descriptionDisplay.innerHTML = data.description
+
+            let forecast1 = `~~~ Tomorrow ~~~  <br> Temperature: ${data.forecast[0].temperature} <br> Wind: ${data.forecast[0].wind} <br><br>`;
+            let forecast2 = `~~~ 2 Days from Now ~~~  <br> Temperature: ${data.forecast[1].temperature}  <br> Wind: ${data.forecast[1].wind} <br><br>`;
+            let forecast3 = `~~~ 3 Days from Now ~~~  <br> Temperature: ${data.forecast[2].temperature} <br> Wind: ${data.forecast[2].wind} <br><br>`;
+            
+            forecastList = [forecast1, forecast2, forecast3];
+
+            forecastList.forEach(forecast => {                
+                let forecastElement = document.createElement('div');
+                forecastElement.innerHTML = forecast;
+                forecastContainer.appendChild(forecastElement);                
+            })
+        }   
     };
